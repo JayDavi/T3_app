2fdf9567b4fd23b15bf449988a8268695782b456
Jabari Davis <jab.davi@gmail.com>
2018-01-05 14:32:45 -0500

    small changes made

--- assets/js/t3scripts.js
+++ assets/js/t3scripts.js
@@ -1,48 +1,70 @@
 
-  function greet(){
-    var name = document.getElementById("name").value;
-    document.getElementById("hello").innerHTML = 'Hello ' + name;
-    document.getElementById("YtR").style.display = 'block';
-    document.getElementById("Income").style.display = 'block';
-  }
+function greet(){
+  var name = document.getElementById("name").value;
+  document.getElementById("hello").innerHTML = 'Hello ' + name;
+  document.getElementById("YtR").style.display = 'block';
+  document.getElementById("Income").style.display = 'block';
+}
 
-  function ageTo65() {
-    var dob = document.getElementById("date").value;
-    if (dob === ''){
-      document.getElementById("theAge").innerHTML = 'Please enter Date of Birth'
-    }
-    else{
+function calAge(){
+  var dob = document.getElementById("date").value;
+  if (dob === ''){
+    document.getElementById("theAge").innerHTML = 'Please enter Date of Birth'
+    return '';
+  }
+  else{
     var age = new Date - new Date(dob);
-    var to65 = 65 - Math.floor(age / 1000 / 60 / 60 / 24 / 365);
-    document.getElementById("theAge").innerHTML = 'You have ' + to65 +
-    ' years until retirement';
-    }
+    return age;
   }
+}
 
-  function calIncome(){
-    var cin = document.getElementById("cincome").value;
-    var ein = document.getElementById("eincome").value;
-    var col = document.getElementById("col").value;
-    var ee = document.getElementById("ee").value;
-    var calcIn = cin - (col*12 + ee*12);
-    var savAmount = getAmount(calcIn);
-    var final = calcIn - savAmount;
-
-    if (cin === '' || ein === ''){
-      document.getElementById("theIncome").innerHTML =
-      'Please enter income(s)';
-    }
-    else{
-    document.getElementById("theIncome").innerHTML = 'You currently make $'
-     + cin + '.<br>You expect to make $'+ ein +' before retirement.<br>'
-     + 'With your entered Expenses you make $' + calcIn +' currently'
-     + '<br>This is what you will put aside based on your choosings $'+savAmount
-     + '. Leaving you with: $' + final;
-    }
+function ageTo65() {
+  var age = calAge();
+  if(age != ''){
+  var to65 = 65 - Math.floor(age / 1000 / 60 / 60 / 24 / 365);
+  document.getElementById("theAge").innerHTML = 'You have ' + to65 +
+  ' years until retirement';
   }
+}
 
-  function getAmount(amountLeft){
-    var savePerc = document.getElementById("percent").value;
-    var calc = amountLeft * savePerc;
-    return calc;
+function calIncome(){
+  var cin = document.getElementById("cincome").value;
+  var ein = document.getElementById("eincome").value;
+  var col = document.getElementById("col").value;
+  var ee = document.getElementById("ee").value;
+  var calcIn = cin - (col*12 + ee*12);
+  var savAmount = getAmount(calcIn);
+  var final = calcIn - savAmount;
+
+  if (cin === '' || ein === ''){
+    document.getElementById("theIncome").innerHTML =
+    'Please enter income(s)';
   }
+  else{
+  document.getElementById("theIncome").innerHTML = 'You currently make $'
+   + cin + '.<br>You expect to make $'+ ein +' before retirement.<br>'
+   + 'With your entered Expenses you make $' + calcIn +' currently'
+   + '<br>This is what you will put aside based on your choosings $'+savAmount
+   + '. Leaving you with: $' + final;
+  }
+}
+
+function getAmount(amountLeft){
+  var savePerc = document.getElementById("percent").value;
+  var calc = Math.floor(amountLeft * savePerc);
+  return calc;
+}
+
+function popRetAgeSel(){
+  var currentAge = Math.floor(calAge() / 1000 / 60 / 60 / 24 / 365);
+  var selString = '';
+  if(currentAge != ''){
+    for(var i=currentAge; i <= 65; i++){
+      selString += '<option value="'+i+'">'+i+'</option>';
+    }
+    document.getElementById("retAge").innerHTML = selString;
+  }
+  else{
+    document.getElementById("retAge").innerHTML = '<option value="65">65</option>';
+  }
+}
