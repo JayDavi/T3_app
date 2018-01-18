8799f9b2fc77561e1d9414f814781dce0e47c84b
Jabari Davis <jab.davi@gmail.com>
2018-01-18 15:58:01 -0500

    added more to the calculations

--- assets/js/t3scripts_1.js
+++ assets/js/t3scripts_1.js
@@ -11,6 +11,9 @@ function showThis(id){
   if (ide === '1'){
     document.getElementById("p"+id).style.display = 'block';
   }
+  else{
+    document.getElementById("p"+id).style.display = 'none';
+  }
 }
 
 function calAge(){
@@ -21,12 +24,12 @@ function calAge(){
   }
   else{
     var age = new Date - new Date(dob);
-    return age;
+    return Math.floor(age / 1000 / 60 / 60 / 24 / 365);
   }
 }
 
 function popRetAgeSel(){
-  var currentAge = Math.floor(calAge() / 1000 / 60 / 60 / 24 / 365);
+  var currentAge = calAge();
   var selString = '';
   if(currentAge != ''){
     for(var i=currentAge; i <= 65; i++){
@@ -39,11 +42,25 @@ function popRetAgeSel(){
   }
 }
 
+function calInflation(num){
+  var re = document.getElementById("retAge").value;
+  var inflation = document.getElementById("inflRate").value;
+  var numToLE = re - calAge();
+  var result = num*Math.pow(1+inflation,numToLE);
+  return Math.round(100*result)/100;
+}
+
+function calInflation2(num, age){
+  var re = document.getElementById("retAge").value;
+  var inflation = document.getElementById("inflRate").value;
+  var numToLE = age - calAge();
+  var result = num*Math.pow(1+inflation,numToLE);
+  return Math.round(100*result)/100;
+}
+
 function section1(){
   var pamAvg = 262000;
-  var le = document.getElementById("LE").value;
-  var numToLE = le - 65;
-  var result = pamAvg / numToLE;
+  var result = calInflation(pamAvg);
   return result;
 }
 
@@ -55,31 +72,56 @@ function section2(){
   var iha = document.getElementById("IHA").value;
   var nhs = document.getElementById("NHS").value;
   var nhp = document.getElementById("NHP").value;
+  var le = document.getElementById("LE").value;
   if(ad === '1'){
-    total += document.getElementById("ageAD").value * 17680;
+    total += calInflation2((le - document.getElementById("ageAD").value) * 17680,
+  document.getElementById("ageAD").value);
   }
   if(al === '1'){
-    total += document.getElementById("ageAL").value * 43539;
+    total += calInflation2((le - document.getElementById("ageAL").value) * 43539,
+  document.getElementById("ageAL").value);
   }
   if(hs === '1'){
-    total += document.getElementById("ageHS").value * 45760;
+    total += calInflation2((le - document.getElementById("ageHS").value) * 45760,
+  document.getElementById("ageHS").value);
   }
   if(iha === '1'){
-    total += document.getElementById("ageIHA").value * 46332;
+    total += calInflation2((le - document.getElementById("ageIHA").value) * 46332,
+  document.getElementById("ageIHA").value);
   }
   if(nhs === '1'){
-    total += document.getElementById("ageNHS").value * 82125;
+    total += calInflation2((le - document.getElementById("ageNHS").value) * 82125,
+  document.getElementById("ageNHS").value);
   }
   if(nhp === '1'){
-    total += document.getElementById("ageNHP").value * 92378;
+    total += calInflation2((le - document.getElementById("ageNHP").value) * 92378,
+  document.getElementById("ageNHP").value);
   }
+
   return total;
+}
+
+function section3(){
+  var total = 0;
+  var rdc = document.getElementById("RDC").value;
+  var ha = document.getElementById("HA").value;
+  var ree = document.getElementById("REE").value;
+  if(rdc === '1'){
+    total += 288;
+  }
+  if(ha === '1'){
+    total += 3000/5;
+  }
+  if(ree === '1'){
+    total += 200;
+  }
+  return calInflation(total);
 }
 
 function totalCost(){
   var income = document.getElementById("income").value;
-  var cost = section1() + section2();
+  var cost = Math.round(100*(section1() + section2() + section3()))/100;
   document.getElementById("theIncome").innerHTML = 'Your income is ' +
   income + '.<br><br> Total HeathCare Cost is $' +
-  cost;
+  cost + '<br>' +section1() + '<br>' +section2() + '<br>' + section3();
 }
